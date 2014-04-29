ff3eb1244c2075da38c356d51b42b866df8dfde4
jeremy1080@gmail.com <jeremy1080@gmail.com@ac8a8a21-f64d-659c-34ae-6444dc4d8953>
2014-04-29 22:44:22 +0000

    Added a GameScreen class that handles rendering of the main game and controls the game logic. Began an Enemy class to instantiate enemies.

--- trunk/build/Debug/MinGW-Windows/main.o.d
+++ trunk/build/Debug/MinGW-Windows/main.o.d
@@ -1,147 +1,148 @@
 build/Debug/MinGW-Windows/main.o: main.cpp \
- ../../../Downloads/SDL2-2.0.3/include/SDL.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_main.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_stdinc.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_config.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_platform.h \
- ../../../Downloads/SDL2-2.0.3/include/begin_code.h \
- ../../../Downloads/SDL2-2.0.3/include/close_code.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_assert.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_atomic.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_audio.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_error.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_endian.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_mutex.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_thread.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_rwops.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_clipboard.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_cpuinfo.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_events.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_video.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_pixels.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_rect.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_surface.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_blendmode.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_keyboard.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_keycode.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_scancode.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_mouse.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_joystick.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_gamecontroller.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_quit.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_gesture.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_touch.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_filesystem.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_haptic.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_hints.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_loadso.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_log.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_messagebox.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_power.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_render.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_system.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_timer.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_version.h \
- ../../../Downloads/SDL2_image-2.0.0/i686-w64-mingw32/include/SDL2/SDL_image.h \
- ../../../Downloads/SDL2-2.0.3/include/SDL_version.h \
- ../../../Downloads/SDL2-2.0.3/include/begin_code.h \
- ../../../Downloads/SDL2-2.0.3/include/close_code.h GraphicsRenderer.h \
- MenuScreen.h
+ ../Libs/SDL2-2.0.3/include/SDL.h ../Libs/SDL2-2.0.3/include/SDL_main.h \
+ ../Libs/SDL2-2.0.3/include/SDL_stdinc.h \
+ ../Libs/SDL2-2.0.3/include/SDL_config.h \
+ ../Libs/SDL2-2.0.3/include/SDL_platform.h \
+ ../Libs/SDL2-2.0.3/include/begin_code.h \
+ ../Libs/SDL2-2.0.3/include/close_code.h \
+ ../Libs/SDL2-2.0.3/include/SDL_assert.h \
+ ../Libs/SDL2-2.0.3/include/SDL_atomic.h \
+ ../Libs/SDL2-2.0.3/include/SDL_audio.h \
+ ../Libs/SDL2-2.0.3/include/SDL_error.h \
+ ../Libs/SDL2-2.0.3/include/SDL_endian.h \
+ ../Libs/SDL2-2.0.3/include/SDL_mutex.h \
+ ../Libs/SDL2-2.0.3/include/SDL_thread.h \
+ ../Libs/SDL2-2.0.3/include/SDL_rwops.h \
+ ../Libs/SDL2-2.0.3/include/SDL_clipboard.h \
+ ../Libs/SDL2-2.0.3/include/SDL_cpuinfo.h \
+ ../Libs/SDL2-2.0.3/include/SDL_events.h \
+ ../Libs/SDL2-2.0.3/include/SDL_video.h \
+ ../Libs/SDL2-2.0.3/include/SDL_pixels.h \
+ ../Libs/SDL2-2.0.3/include/SDL_rect.h \
+ ../Libs/SDL2-2.0.3/include/SDL_surface.h \
+ ../Libs/SDL2-2.0.3/include/SDL_blendmode.h \
+ ../Libs/SDL2-2.0.3/include/SDL_keyboard.h \
+ ../Libs/SDL2-2.0.3/include/SDL_keycode.h \
+ ../Libs/SDL2-2.0.3/include/SDL_scancode.h \
+ ../Libs/SDL2-2.0.3/include/SDL_mouse.h \
+ ../Libs/SDL2-2.0.3/include/SDL_joystick.h \
+ ../Libs/SDL2-2.0.3/include/SDL_gamecontroller.h \
+ ../Libs/SDL2-2.0.3/include/SDL_quit.h \
+ ../Libs/SDL2-2.0.3/include/SDL_gesture.h \
+ ../Libs/SDL2-2.0.3/include/SDL_touch.h \
+ ../Libs/SDL2-2.0.3/include/SDL_filesystem.h \
+ ../Libs/SDL2-2.0.3/include/SDL_haptic.h \
+ ../Libs/SDL2-2.0.3/include/SDL_hints.h \
+ ../Libs/SDL2-2.0.3/include/SDL_loadso.h \
+ ../Libs/SDL2-2.0.3/include/SDL_log.h \
+ ../Libs/SDL2-2.0.3/include/SDL_messagebox.h \
+ ../Libs/SDL2-2.0.3/include/SDL_power.h \
+ ../Libs/SDL2-2.0.3/include/SDL_render.h \
+ ../Libs/SDL2-2.0.3/include/SDL_system.h \
+ ../Libs/SDL2-2.0.3/include/SDL_timer.h \
+ ../Libs/SDL2-2.0.3/include/SDL_version.h \
+ ../Libs/i686-w64-mingw32/include/SDL2/SDL_image.h \
+ ../Libs/SDL2-2.0.3/include/SDL_version.h \
+ ../Libs/SDL2-2.0.3/include/begin_code.h \
+ ../Libs/SDL2-2.0.3/include/close_code.h GraphicsRenderer.h MenuScreen.h \
+ GameScreen.h
 
-../../../Downloads/SDL2-2.0.3/include/SDL.h:
+../Libs/SDL2-2.0.3/include/SDL.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_main.h:
+../Libs/SDL2-2.0.3/include/SDL_main.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_stdinc.h:
+../Libs/SDL2-2.0.3/include/SDL_stdinc.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_config.h:
+../Libs/SDL2-2.0.3/include/SDL_config.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_platform.h:
+../Libs/SDL2-2.0.3/include/SDL_platform.h:
 
-../../../Downloads/SDL2-2.0.3/include/begin_code.h:
+../Libs/SDL2-2.0.3/include/begin_code.h:
 
-../../../Downloads/SDL2-2.0.3/include/close_code.h:
+../Libs/SDL2-2.0.3/include/close_code.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_assert.h:
+../Libs/SDL2-2.0.3/include/SDL_assert.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_atomic.h:
+../Libs/SDL2-2.0.3/include/SDL_atomic.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_audio.h:
+../Libs/SDL2-2.0.3/include/SDL_audio.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_error.h:
+../Libs/SDL2-2.0.3/include/SDL_error.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_endian.h:
+../Libs/SDL2-2.0.3/include/SDL_endian.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_mutex.h:
+../Libs/SDL2-2.0.3/include/SDL_mutex.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_thread.h:
+../Libs/SDL2-2.0.3/include/SDL_thread.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_rwops.h:
+../Libs/SDL2-2.0.3/include/SDL_rwops.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_clipboard.h:
+../Libs/SDL2-2.0.3/include/SDL_clipboard.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_cpuinfo.h:
+../Libs/SDL2-2.0.3/include/SDL_cpuinfo.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_events.h:
+../Libs/SDL2-2.0.3/include/SDL_events.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_video.h:
+../Libs/SDL2-2.0.3/include/SDL_video.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_pixels.h:
+../Libs/SDL2-2.0.3/include/SDL_pixels.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_rect.h:
+../Libs/SDL2-2.0.3/include/SDL_rect.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_surface.h:
+../Libs/SDL2-2.0.3/include/SDL_surface.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_blendmode.h:
+../Libs/SDL2-2.0.3/include/SDL_blendmode.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_keyboard.h:
+../Libs/SDL2-2.0.3/include/SDL_keyboard.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_keycode.h:
+../Libs/SDL2-2.0.3/include/SDL_keycode.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_scancode.h:
+../Libs/SDL2-2.0.3/include/SDL_scancode.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_mouse.h:
+../Libs/SDL2-2.0.3/include/SDL_mouse.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_joystick.h:
+../Libs/SDL2-2.0.3/include/SDL_joystick.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_gamecontroller.h:
+../Libs/SDL2-2.0.3/include/SDL_gamecontroller.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_quit.h:
+../Libs/SDL2-2.0.3/include/SDL_quit.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_gesture.h:
+../Libs/SDL2-2.0.3/include/SDL_gesture.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_touch.h:
+../Libs/SDL2-2.0.3/include/SDL_touch.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_filesystem.h:
+../Libs/SDL2-2.0.3/include/SDL_filesystem.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_haptic.h:
+../Libs/SDL2-2.0.3/include/SDL_haptic.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_hints.h:
+../Libs/SDL2-2.0.3/include/SDL_hints.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_loadso.h:
+../Libs/SDL2-2.0.3/include/SDL_loadso.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_log.h:
+../Libs/SDL2-2.0.3/include/SDL_log.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_messagebox.h:
+../Libs/SDL2-2.0.3/include/SDL_messagebox.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_power.h:
+../Libs/SDL2-2.0.3/include/SDL_power.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_render.h:
+../Libs/SDL2-2.0.3/include/SDL_render.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_system.h:
+../Libs/SDL2-2.0.3/include/SDL_system.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_timer.h:
+../Libs/SDL2-2.0.3/include/SDL_timer.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_version.h:
+../Libs/SDL2-2.0.3/include/SDL_version.h:
 
-../../../Downloads/SDL2_image-2.0.0/i686-w64-mingw32/include/SDL2/SDL_image.h:
+../Libs/i686-w64-mingw32/include/SDL2/SDL_image.h:
 
-../../../Downloads/SDL2-2.0.3/include/SDL_version.h:
+../Libs/SDL2-2.0.3/include/SDL_version.h:
 
-../../../Downloads/SDL2-2.0.3/include/begin_code.h:
+../Libs/SDL2-2.0.3/include/begin_code.h:
 
-../../../Downloads/SDL2-2.0.3/include/close_code.h:
+../Libs/SDL2-2.0.3/include/close_code.h:
 
 GraphicsRenderer.h:
 
 MenuScreen.h:
+
+GameScreen.h:
